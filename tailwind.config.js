532248a651872b1dd68c36f5263815feb6f07f69
Escher WD <escherwd@icloud.com>
2024-02-15 19:13:25 -0800

    Home page updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,5 +12,13 @@ module.exports = {
 			display: ['Inter', 'sans-serif'],
 			mono: ['monospace'],
 		},
+		fontSize: {
+			base: '14px',
+			sm: '12px',
+			xs: '10px',
+			h3: '24px',
+			h2: '48px',
+			h1: '64px',
+		}
 	}
 };
