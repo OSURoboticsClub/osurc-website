eac682f8a98587a0f83e4531efa588c6f1115097
Escher WD <escherwd@yahoo.com>
2024-04-08 13:20:45 -0700

    give navbar a background color on scroll

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,10 @@
 // App JavaScript can go here
+
+window.addEventListener('scroll', (_, ev) => {
+    // console.log(window.scrollY);
+    if (window.scrollY > 10) {
+        document.getElementById("navbar").classList.add("bg-neutral-900");
+    } else {
+        document.getElementById("navbar").classList.remove("bg-neutral-900");
+    }
+})
--- static/js/app.js
+++ static/js/app.js
@@ -9,6 +9,15 @@
 
 // App JavaScript can go here
 
+window.addEventListener('scroll', function (_, ev) {
+  // console.log(window.scrollY);
+  if (window.scrollY > 10) {
+    document.getElementById("navbar").classList.add("bg-neutral-900");
+  } else {
+    document.getElementById("navbar").classList.remove("bg-neutral-900");
+  }
+});
+
 /***/ }),
 
 /***/ "./assets/css/styles.scss":
