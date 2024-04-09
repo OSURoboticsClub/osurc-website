dea4225a58123d0b5b7bcf20a21edf4616f2f299
Escher WD <escherwd@yahoo.com>
2024-04-09 13:54:03 -0700

    mobile styles

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,3 +8,26 @@ window.addEventListener('scroll', (_, ev) => {
         document.getElementById("navbar").classList.remove("bg-neutral-900");
     }
 })
+
+let menuOpen = false;
+document.getElementById("menuToggle").addEventListener("click", () => {
+    console.log("menu open click")
+    if (menuOpen) {
+        // Close the menu
+        document.getElementById("mobileMenu").classList.add("hidden");
+        document.getElementById("mobileMenuCloseSvg").classList.add("hidden");
+        document.getElementById("mobileMenuOpenSvg").classList.remove("hidden");
+        menuOpen = false;
+        if (window.scrollY < 10) {
+            document.getElementById("navbar").classList.remove("bg-neutral-900");
+        }
+    } else {
+        // Open the menu
+        document.getElementById("mobileMenu").classList.remove("hidden");
+        document.getElementById("mobileMenuCloseSvg").classList.remove("hidden");
+        document.getElementById("mobileMenuOpenSvg").classList.add("hidden");
+        document.getElementById("navbar").classList.add("bg-neutral-900");
+        menuOpen = true;
+    }
+});
+
--- static/js/app.js
+++ static/js/app.js
@@ -17,6 +17,27 @@ window.addEventListener('scroll', function (_, ev) {
     document.getElementById("navbar").classList.remove("bg-neutral-900");
   }
 });
+var menuOpen = false;
+document.getElementById("menuToggle").addEventListener("click", function () {
+  console.log("menu open click");
+  if (menuOpen) {
+    // Close the menu
+    document.getElementById("mobileMenu").classList.add("hidden");
+    document.getElementById("mobileMenuCloseSvg").classList.add("hidden");
+    document.getElementById("mobileMenuOpenSvg").classList.remove("hidden");
+    menuOpen = false;
+    if (window.scrollY < 10) {
+      document.getElementById("navbar").classList.remove("bg-neutral-900");
+    }
+  } else {
+    // Open the menu
+    document.getElementById("mobileMenu").classList.remove("hidden");
+    document.getElementById("mobileMenuCloseSvg").classList.remove("hidden");
+    document.getElementById("mobileMenuOpenSvg").classList.add("hidden");
+    document.getElementById("navbar").classList.add("bg-neutral-900");
+    menuOpen = true;
+  }
+});
 
 /***/ }),
 
